f91b5d43dcdae7621893b3e6f8eefdb8e8dfb637
Halil Karadayı <halil-atlassian-fdct6v@deepinfo.com>
2024-11-10 19:32:32 +0300

    added.

--- test/app.test.js
+++ test/app.test.js
@@ -1,20 +1,21 @@
 // test/app.test.js
-const assert = require('chai').assert;
-const add = require('../app');  // app.js'deki add fonksiyonunu içeri aktar
+const assert = require('assert');
+const add = require('../nodejs-ci-cd-example/app');  // app.js'deki add fonksiyonunu içeri aktar
 
 describe('Add Function', function() {
     it('should return 5 when 2 and 3 are added', function() {
         const result = add(2, 3);
-        assert.equal(result, 5);  // Beklenen sonuç: 5
+        assert.strictEqual(result, 5);  // Beklenen sonuç: 5
     });
 
     it('should return 0 when -1 and 1 are added', function() {
         const result = add(-1, 1);
-        assert.equal(result, 0);  // Beklenen sonuç: 0
+        assert.strictEqual(result, 0);  // Beklenen sonuç: 0
     });
 
     it('should return -5 when -2 and -3 are added', function() {
         const result = add(-2, -3);
-        assert.equal(result, -5);  // Beklenen sonuç: -5
+        assert.strictEqual(result, -5);  // Beklenen sonuç: -5
     });
 });
+
